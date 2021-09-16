a4bc6ad7c64a05aa0a9dc9dafaf16ac737f60cc1
angelofallars <ba.fallaria@gmail.com>
2021-09-17 06:42:07 +0800

    Refactor 2 and 3

--- main.py
+++ main.py
@@ -18,10 +18,21 @@ def clear():
         system("clear")
 
 
-def printlist(cursor):
+def id_from_input(task_ids):
+    """Get an ID of a task from user input"""
+    index = int(input("$ ")) - 1
+    return task_ids[index]
+
+
+def print_list(cursor, numbering=False):
     """Print the current todo list.
 
-    Returns the IDs of the tasks ordered in a list."""
+    Returns the IDs of the tasks ordered in a list.
+
+    Args:
+        cursor - the SQL cursor
+        numbering - add numbering to the displayed list"""
+
     task_ids = []
 
     # Select the list of tasks
@@ -30,15 +41,27 @@ def printlist(cursor):
     # Fetch the results
     rows = cursor.fetchall()
 
+    # Display numbers if needed to print numbers of list
+    number = 1
+
     for row in rows:
         task_ids.append(row[0])
+
+        if numbering:
+            print("{}. ".format(number), end="")
+            number += 1
 
         if row[3]:
             print("[x] ", end="")
         else:
             print("[ ] ", end="")
 
-        print("{}\n    {}".format(row[1], row[2]))
+        # Print the title
+        print("{}".format(row[1]))
+
+        # Print the description (Don't print blank descriptions)
+        if row[2] != "":
+            print("    {}".format(row[2]))
 
     return task_ids
 
@@ -63,15 +86,9 @@ def main():
                        """)
 
         # First task
-        cur.execute("""INSERT INTO tasklist(title, description)
-                       VALUES('Clean the room',
-                              'You need to clean your room now!')""")
-        # Second task
-        cur.execute("""INSERT INTO tasklist(title, description, finished)
-                       VALUES('Do the biology homework', NULL, 1)""")
-        # Third task
-        cur.execute("""INSERT INTO tasklist(title, description)
-                       VALUES('Make a sandwich', NULL)""")
+        # cur.execute("""INSERT INTO tasklist(title, description)
+        #               VALUES('Clean the room',
+        #                      'You need to clean your room now!')""")
 
         # Save and close the database
         con.commit()
@@ -82,12 +99,13 @@ def main():
 
     while True:
         clear()
+
         print("===== TODO LIST BY ANGELO-F =====")
+        task_ids = print_list(cur)
+        print("=================================")
+        option = input("[1] Add [2] Delete [3] Mark as done [4] Exit\n$ ")
 
-        task_ids = printlist(cur)
-
-        option = input("[1] Add [2] Delete [3] Exit\n$ ")
-
+        # Add
         if option == '1':
             title = input("Title of task: ")
             description = input("Description of task (Can be blank): ")
@@ -95,16 +113,35 @@ def main():
             cur.execute("""INSERT INTO tasklist(title, description)
                            VALUES(?, ?)""", (title, description))
 
+        # Delete
         elif option == '2':
+            clear()
+            print("===== TODO LIST BY ANGELO-F =====")
+            task_ids = print_list(cur, numbering=True)
+            print("=================================")
+
             print("Which task to delete?")
-            deleted_index = int(input("$ ")) - 1
-            id_to_delete = task_ids[deleted_index]
-            print(id_to_delete)
+            id_to_delete = id_from_input(task_ids)
 
             cur.execute("""DELETE FROM tasklist WHERE id = ?""",
                         (id_to_delete, ))
 
+        # Mark as done
         elif option == '3':
+            clear()
+            print("===== TODO LIST BY ANGELO-F =====")
+            task_ids = print_list(cur, numbering=True)
+            print("=================================")
+
+            print("Which task to mark as done/undone?")
+            id_to_mark = id_from_input(task_ids)
+
+            cur.execute("""UPDATE tasklist
+                           SET finished = NOT finished
+                           WHERE id = ?""", (id_to_mark, ))
+
+        # Exit
+        elif option == '4':
             print("Thanks for running my program!")
             break
 
