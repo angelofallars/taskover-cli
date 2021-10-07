f39fedf1aa03f1631997b1f786a14c3cf9ecc493
Angelo Fallaria <ba.fallaria@gmail.com>
2021-10-07 10:46:49 +0800

    Fix kb.py for issues

--- kb.py
+++ kb.py
@@ -1,15 +1,27 @@
 """Instant input with keyboard"""
-import sys, termios, tty, os, time
+import sys
+import termios
+import tty
+import os
+import time
  
+
 def getch():
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
- 
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
- 
+    # Unix (Linux / macOS)
+    if os.name != "nt":
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+    
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+    # Windows
+    else:
+        # TODO: !!! Add the Windows-specific code here !!!
+        pass
+
+
 button_delay = 0.2
